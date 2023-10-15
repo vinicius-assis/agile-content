b35f1fd142b9c71ff65c69c27507492597594e17
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-10-15 18:07:52 -0300

    feat: create Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Header from "./components/Header/Header";
 import MainIcon from "./components/MainIcon/MainIcon";
+import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
     <>
+      <Header>
+        <h1>
+          <strong>Agile Content</strong> Frontend test
+        </h1>
+        <Menu />
+      </Header>
       <MainIcon />
       <h1>Teste</h1>
     </>
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Menu = () => {
+  const items = new Array(9).fill("");
+  return (
+    <ul className="menu">
+      {items.map((_, index) => (
+        <li key={index}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default Menu;
