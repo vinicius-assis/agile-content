2db87a8ebb05783ebd489f121f0e5845aab7096d
Vinicius Assis <viniciusassis.dev@hotmail.com>
2023-10-17 23:27:11 -0300

    feat: add link in Google icon to return to home page

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header/Header";
 import { GoogleIcon } from "../components/Icons/Icons";
 import ResultSection from "../components/ResultSection/ResultSection";
@@ -7,7 +8,9 @@ const ResultPage = () => {
   return (
     <>
       <Header className="-result-page">
-        <GoogleIcon width="92" height="30" />
+        <Link to="/">
+          <GoogleIcon width="92" height="30" />
+        </Link>
         <SearchInputGroup />
       </Header>
       <div className="container -result">
